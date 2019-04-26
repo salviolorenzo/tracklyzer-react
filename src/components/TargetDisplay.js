04e8f59969280b67caec44fad76b30a0464b8871
salviolorenzo <salviolorenzo@gmail.com>
2019-04-26 16:53:32 -0400

    adding build

---
+++ src/components/TargetDisplay.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import ExtendedInfo from './ExtendedInfo';
+import Performance from './Performance';
+import { Route, NavLink } from 'react-router-dom';
+import SwipeableRoutes from 'react-swipeable-routes';
+
+const targetDisplay = props => {
+  const activeLinkStyle = {
+    backgroundColor: 'rgba(0, 140, 255, 0.7)',
+    color: 'white',
+    padding: '5px 10px',
+    borderRadius: '20px'
+  };
+
+  if (props.isMobile) {
+    return (
+      <div className="infoContainer">
+        <ul className="infoLinks">
+          <li>
+            <NavLink
+              onClick={props.handleClickedLink}
+              to="/information"
+              activeStyle={activeLinkStyle}
+            >
+              Information
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              onClick={props.handleClickedLink}
+              to="/performance"
+              activeStyle={activeLinkStyle}
+            >
+              Performance
+            </NavLink>
+          </li>
+        </ul>
+        <SwipeableRoutes>
+          <Route
+            path="/information"
+            render={routeProps => {
+              return (
+                <ExtendedInfo
+                  item={props.item}
+                  handleEditClick={props.handleEditClick}
+                  handleItemDelete={props.handleItemDelete}
+                  {...props}
+                />
+              );
+            }}
+          />
+
+          <Route
+            path="/performance"
+            render={routeProps => {
+              return <Performance item={props.item} {...props} />;
+            }}
+          />
+        </SwipeableRoutes>
+      </div>
+    );
+  } else {
+    return (
+      <div className="infoContainer">
+        <ExtendedInfo
+          item={props.item}
+          handleEditClick={props.handleEditClick}
+          handleItemDelete={props.handleItemDelete}
+        />
+        <Performance item={props.item} />
+      </div>
+    );
+  }
+};
+
+export default targetDisplay;

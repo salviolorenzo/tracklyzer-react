688667d2c1910e590809c1f1e389394200b32d0a
salviolorenzo <salviolorenzo@gmail.com>
2019-04-29 13:16:00 -0400

    adding test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TestUtils from 'react-dom/test-utils'; // ES6
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+
+  it('renders an h1', function() {
+    var component = TestUtils.renderIntoDocument(<App />, div);
+
+    var h1 = TestUtils.findRenderedDOMComponentWithTag(component, 'h1');
+
+    expect(h1.getDOMNode().textContent).toEqual('Tracklyzer');
+  });
 });
